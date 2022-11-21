1ac7eea5c32f1850a6b167f4d7fe28698949b48a
xxq <417848079@qq.com>
2022-11-21 18:00:41 +0800

    查询文件初始化

--- mysql/stu-select-study.sql
+++ mysql/stu-select-study.sql
@@ -1,2 +1,29 @@
 SHOW TABLES;
-SELECT * FROM `stu`
+
+-- 查询所有数据
+SELECT
+	* 
+FROM
+	stu;
+
+
+-- 给指定列添加数据
+INSERT INTO stu(id,name) VALUES (3,'张三');
+
+
+-- 给所有列添加数据 (批量添加)
+INSERT INTO stu
+(id	-- 编号	
+,name,gender,birthday,score,email,tel,status) VALUES(5,'李四','男','1999-11-11','88.88','testfja@qq.com','111000',1);
+
+INSERT INTO stu VALUES(2,'李四','男','1999-11-11','88.88','testfja@qq.com','111000',1);
+
+
+-- 修改数据
+UPDATE stu set gender ='男' WHERE id = 1;
+
+UPDATE stu set birthday = '1999-12-12', score = 99.99 WHERE id = 1;
+
+
+-- 删除数据
+DELETE FROM stu WHERE id = '5';
---
+++ mysql/stu2-select.sql
@@ -0,0 +1,16 @@
+-- 创建stu2表
+CREATE TABLE stu2 (
+	id INT,	-- 编号
+	name VARCHAR, -- 姓名
+	age INT, -- 年龄
+	sex VARCHAR, -- 性别
+	address VARCHAR(100), -- 地址
+	math DOUBLE(5,2), -- 数学成绩
+	english DOUBLE(5,2), -- 英语成绩
+	hire_date date, -- 入学时间
+)
+
+
+-- 添加数据
+INSERT INTO sty2(id,NAME,age,sex,address,math,english,)
+
