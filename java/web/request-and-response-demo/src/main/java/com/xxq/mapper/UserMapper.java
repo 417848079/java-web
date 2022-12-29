3faae87c59d345fffebc2a805eacfebb039de413
xxq <417848079@qq.com>
2022-12-29 17:35:53 +0800

    2022-12-29日学习

--- java/web/request-and-response-demo/src/main/java/com/xxq/mapper/UserMapper.java
+++ java/web/request-and-response-demo/src/main/java/com/xxq/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.xxq.mapper;
 
 import com.xxq.pojo.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,6 +15,20 @@ public interface UserMapper {
      * @param: passWord - [String]
      * @return: com.xxq.pojo.User
      */
-    @Select("select * tb_user where ususername =#{userName} and password=#{passWord}}")
+    @Select("select * from tb_user where username =#{userName} and password=#{passWord}")
     User Select(@Param("userName") String userName, @Param("passWord") String passWord);
+
+    /**
+     * @author: amw
+     * @createTime: 2022年12月29 11:04:12
+     * @description:根据用户名查询用户对象
+     * @param: userName - [String]
+     * @return: com.xxq.pojo.User
+     */
+    @Select("select * from tb_user where username=#{userName}")
+    User SelectByUserName(String userName);
+
+
+//    @Insert( )
+//    int AddUser(@Param("userName") String userName, @Param("passWord") String passWord);
 }
---
+++ java/web/request-and-response-demo/src/main/java/com/xxq/web/RegisterServlet.java
@@ -0,0 +1,22 @@
+package com.xxq.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+        System.out.println("111");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        this.doGet(request, response);
+    }
+}
